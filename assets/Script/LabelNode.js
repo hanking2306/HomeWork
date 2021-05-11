6184238e7764a1c83f7a00c01275416b52276cbe
Han King <hanking2306@gmail.com>
2021-05-11 19:58:39 +0700

    Fix Bug Complete

--- assets/Script/LabelNode.js
+++ assets/Script/LabelNode.js
@@ -13,89 +13,110 @@ cc.Class({
         checkPass: false,
     },
 
-    onLoad () {
+    onLoad() {
         this.enoughUser.node.active = false;
-        this.userNote.node.active = false;
-        this.passWordNote.node.active = false;
-        this.ErrorUserName.node.active = false;
-        this.ErrorPassWord.node.active = false;
+        this.hideNoteUser();
+        this.hideNotePw();
         Emitter.instance.registerEvent("showRemind", this.onShowRemind.bind(this));
         Emitter.instance.registerEvent("editEnd", this.onEditEnd.bind(this));
         Emitter.instance.registerEvent("showNotify", this.onShowNotify.bind(this));
         Emitter.instance.registerEvent("stopResgiter", this.onStopRegister.bind(this));
     },
 
-    onShowRemind(nameBox){
-        if(nameBox == "UserName"){
+    onShowRemind(nameBox) {
+        if (nameBox == "UserName") {
             this.userNote.node.active = true;
-            this.passWordNote.node.active = false;
+            this.hideNotePw();
         }
         else {
-            this.userNote.node.active = false;
             this.passWordNote.node.active = true;
+            this.hideNoteUser();
         }
     },
 
-    onEditEnd(nameBox){
+    hideNoteUser(){
+        this.userNote.node.active = false;
+        this.ErrorUserName.node.active = false;
+    },
+
+    hideNotePw(){
+        this.passWordNote.node.active = false;
+        this.ErrorPassWord.node.active = false;
+    },
+
+    bugUser() {
+        this.checkUser = false;
+        this.ErrorUserName.node.active = true;
+        this.userNote.node.active = true;
+    },
+
+    bugPassWord(){
+        this.checkPass = false;
+        this.ErrorPassWord.node.active = true;
+        this.passWordNote.node.active = true;
+    },
+
+    onEditEnd(nameBox) {
         var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
         var upperCase = /[A-Z]/;
         var lowerCase = /[a-z]/;
         var numberCase = /[0-9]/;
-        if(nameBox.node.name == "UserName"){
-            if(format.test(nameBox.string)){
-                this.userNote.node.active = true;
-                this.ErrorUserName.node.active = true;
+        if (nameBox.node.name == "UserName") {
+            if (nameBox.string == "") {
+                this.bugUser();
+            }
+            else if (format.test(nameBox.string)) {
+                this.bugUser();
             }
             else {
+                this.hideNoteUser();
                 this.checkUser = true;
             }
         }
         else {
-            if(nameBox.string.length < 6){
-                this.ErrorPassWord.node.active = true;
-                this.passWordNote.node.active = true;
+            if (nameBox.string.length < 6) {
+                this.bugPassWord();
             }
-            if(!upperCase.test(nameBox.string)){
-                this.ErrorPassWord.node.active = true;
-                this.passWordNote.node.active = true;
+            else if (!upperCase.test(nameBox.string)) {
+                this.bugPassWord();
             }
-            if(!lowerCase.test(nameBox.string)){
-                this.ErrorPassWord.node.active = true;
-                this.passWordNote.node.active = true;
+            else if (!lowerCase.test(nameBox.string)) {
+                this.bugPassWord();
             }
-            if(!numberCase.test(nameBox.string)){
-                this.ErrorPassWord.node.active = true;
-                this.passWordNote.node.active = true;
+            else if (!numberCase.test(nameBox.string)) {
+                this.bugPassWord();
             }
             else {
+                this.hideNotePw();
                 this.checkPass = true;
             }
         }
     },
 
-    onShowNotify(userString){
-        var daytime = new Date(); 
+    onShowNotify(userString) {
+        var daytime = new Date();
         var hour = daytime.getHours();
         var minute = daytime.getMinutes();
         var sencond = daytime.getSeconds();
         hour = (hour < 10) ? ("0" + hour) : hour;
         minute = (minute < 10) ? ("0" + minute) : minute;
         sencond = (sencond < 10) ? ("0" + sencond) : sencond;
-        if(this.checkUser === true && this.checkPass === true){
+        if (this.checkUser === true && this.checkPass === true) {
             var itemUser = userString + " : " + hour + ":" + minute + ":" + sencond;
-            this.noticeHappy.string = "Chào mừng " + "<color=#ff0000><u>" + userString + "</u></color>" 
-                                    + " đã gia nhập lúc " + "<color=#fff000><i>" + hour + ":" + minute + ":" + sencond + "</i></color>";
+            this.noticeHappy.string = "Chào mừng " + "<color=#ff0000><u>" + userString + "</u></color>"
+                + " đã gia nhập lúc " + "<color=#fff000><i>" + hour + ":" + minute + ":" + sencond + "</i></color>";
             Emitter.instance.emit('addItem', itemUser);
+            this.checkUser = false;
+            this.checkPass = false;
         }
     },
 
-    onStopRegister(){
+    onStopRegister() {
         this.enoughUser.node.active = true;
-        this.passWordNote.node.active = false;
         Emitter.instance.emit('hideEditBox');
     },
 
-    start () {
+    start() {
 
     },
 
